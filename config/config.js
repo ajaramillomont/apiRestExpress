d31a08c4bf90cfa7f5a198a5e1867d1c549ef051
ajaramillomont <ajaramillomont@gmail.com>
2025-01-27 19:08:35 -0500

    Deploy

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbnName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
+  dbUser: process.env.DB_USER || 'postgres',
+  dbPassword: process.env.DB_PASSWORD || 'ANDres1988',
+  dbHost: process.env.DB_HOST || 'postgresql',
+  dbnName: process.env.DB_NAME || 'tienda',
+  dbPort: process.env.DB_PORT || '5432',
 }
 
 module.exports = { config };
