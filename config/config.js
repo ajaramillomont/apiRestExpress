47e7a5a4ff004724eb4a353ddd1c75ca6d4a472c
ajaramillomont <ajaramillomont@gmail.com>
2025-01-28 10:36:39 -0500

    cambiando dirección de host

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
   port: process.env.PORT,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
+  dbHost: process.env.DATABASE_URL,
   dbnName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
 }
